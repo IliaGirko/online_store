a0542045225bbabda41cf5d32f2eb26864b22cb4
Илья Гирько <Gggggirr@gmail.com>
2024-12-05 17:53:34 +0300

    Migration 0001 complited

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
+    image = models.ImageField(verbose_name="Фото", null=True, blank=True)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="category", verbose_name="Категория", null=True, blank=True,)
+    price = models.IntegerField(verbose_name="Цена")
+    created_at = models.DateTimeField(verbose_name="Дата создания")
+    updated_at = models.DateTimeField(verbose_name="Дата последнего изменения")
+
+    def __str__(self):
+        return f"Наименование - {self.name}, стоимость = {self.price}"
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
+
+    def __str__(self):
+        return f"Категория {self.name}"
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
