a0b709ffa5f1aee44bfe82015ea7f755b26e6ee9
Илья Гирько <Gggggirr@gmail.com>
2024-12-10 15:14:27 +0300

    Init branch feature/task03

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
+    path("get_product/", views.get_product, name="get_product"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product, Category
 
 def home(request):
     return render(request, "home.html")
@@ -10,3 +11,10 @@ def contacts(request):
         name = request.POST.get("name")
         return HttpResponse(f"{name} спасибо, за обращение. Мы свяжемся с Вами в ближайшее время.")
     return render(request, "contacts.html")
+
+
+def get_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, "product.html", context)
+
